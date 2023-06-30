f3db82494b8f5bd72fc76c49d45def288ea2005f
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-06-30 09:08:18 +0800

    更改 - 更新 version 版本 - 变成 asset 模块中显示效果

--- miniprogram/pages/asset/asset.ts
+++ miniprogram/pages/asset/asset.ts
@@ -2,6 +2,16 @@
 Page({
 
     /**
+     * 将数据库内存储的长时间格式去除 T .000Z 转换为正常显示时间格式
+     * @param time 返回正常显示时间
+     */
+    dateFunction(time: string | number | Date) {
+        var zoneDate = new Date(time).toJSON();
+        var date = new Date(+new Date(zoneDate) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '');
+        return date;
+    },
+
+    /**
      * 页面的初始数据
      */
     data: {
@@ -17,6 +27,7 @@ Page({
         // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据F
         eventChannel.on('indexToAssetPages', function (res) {
             // console.log('资产获取成功：', res.data);
+            res.data.update_time = that.dateFunction(res.data.update_time)
             that.setData({
                 asset: res.data
             })
